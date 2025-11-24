77c9f3b98e1e2f964ac31ac2643bad7807b50cc2
daigotechcareer-arch <daigo.tech.career@gmail.com>
2025-11-24 09:42:25 +0900

    UserとTripモデルに存在性および数値のバリデーションを追加

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,6 +1,16 @@
 # app/models/trip.rb
 
 class Trip < ApplicationRecord
+  # 存在性の検証（必須項目）
+  validates :title, presence: true
+  validates :start_date, presence: true
+  validates :total_budget, presence: true
+  validates :travel_theme, presence: true
+
+  # データ型の検証 (total_budgetは数値である必要がある)
+  validates :total_budget, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+
   # 旅程の作成者（オーナー）とのカスタム関連付け
   belongs_to :owner, class_name: 'User', foreign_key: 'owner_id'
 
