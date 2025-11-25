f146a067fac8e641218fb8972c56e00d2c1a0d9a
daigotechcareer-arch <daigo.tech.career@gmail.com>
2025-11-25 14:10:06 +0900

    スポット登録機能（Controller/View）を実装

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -1,10 +1,36 @@
+# app/controllers/spots_controller.rb
+
 class SpotsController < ApplicationController
-  def create
+  before_action :authenticate_user!
+  before_action :set_trip
+
+  def new
+    @spot = @trip.spots.build
   end
 
-  def update
+  def create
+    @spot = @trip.spots.build(spot_params)
+    
+    if @spot.save
+      redirect_to trip_path(@trip), notice: 'スポットを追加しました！'
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    @spot = @trip.spots.find(params[:id])
+    @spot.destroy
+    redirect_to trip_path(@trip), notice: 'スポットを削除しました。', status: :see_other
+  end
+
+  private
+
+  def set_trip
+    @trip = Trip.shared_with_user(current_user).find(params[:trip_id])
+  end
+
+  def spot_params
+    params.require(:spot).permit(:name, :day_number, :estimated_cost, :duration, :booking_url)
   end
 end
