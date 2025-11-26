e3f1892fc990bb5e2d80c9d435453a520ff93ba7
daigotechcareer-arch <daigo.tech.career@gmail.com>
2025-11-26 11:37:44 +0900

    fix: TripsControllerにedit, update, destroyアクションを追加してコールバックエラーを解消

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,9 +1,7 @@
-# app/controllers/trips_controller.rb
-
 class TripsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_trip, only: [:show, :edit, :update, :destroy]
-  
+
   def index
     @trips = Trip.shared_with_user(current_user)
   end
@@ -14,9 +12,9 @@ class TripsController < ApplicationController
 
   def create
     @trip = current_user.owned_trips.build(trip_params)
-    
+
     if @trip.save
-      # 成功時: 作成された旅程の詳細画面へリダイレクト（routes.rbより@tripに自動対応）
+      # 成功時: 作成された旅程の詳細画面へリダイレクト
       redirect_to @trip, notice: '新しい旅程が作成されました。AIに詳細を相談しましょう！'
     else
       # 失敗時: 再度新規作成フォームを表示
@@ -27,10 +25,26 @@ class TripsController < ApplicationController
 
   def show
   end
-  
+
+  def edit
+  end
+
+  def update
+    if @trip.update(trip_params)
+      redirect_to @trip, notice: '旅程を更新しました。'
+    else
+      flash.now[:alert] = '更新に失敗しました。入力内容を確認してください。'
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @trip.destroy
+    redirect_to trips_path, notice: '旅程を削除しました。', status: :see_other
+  end
+
   private
-  
-  # Strong Parameters: DBスキーマで定義した必須項目のみを許可
+
   def trip_params
     params.require(:trip).permit(:title, :start_date, :total_budget, :travel_theme)
   end
