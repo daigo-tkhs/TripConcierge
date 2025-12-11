287fcce49c786ee7d4b34fba7d14920254369813
daigotechcareer-arch <daigo.tech.career@gmail.com>
2025-12-11 14:40:52 +0900

    Fix: AddSenderToTripInvitationsマイグレーションをNULL安全な三段階方式に修正

--- db/migrate/20251211051838_add_sender_to_trip_invitations.rb
+++ db/migrate/20251211051838_add_sender_to_trip_invitations.rb
@@ -1,5 +1,20 @@
+# db/migrate/[タイムスタンプ]_add_sender_to_trip_invitations.rb
+
 class AddSenderToTripInvitations < ActiveRecord::Migration[7.1]
-  def change
-    add_reference :trip_invitations, :sender, null: false, foreign_key: true
+  def up
+    add_reference :trip_invitations, :sender, foreign_key: { to_table: :users }
+
+    first_user_id = ActiveRecord::Base.connection.execute("SELECT id FROM users ORDER BY id ASC LIMIT 1;").first['id']
+    
+    if first_user_id
+      execute "UPDATE trip_invitations SET sender_id = #{first_user_id} WHERE sender_id IS NULL;"
+    end
+
+    # 3. カラムの制約を NULL 不可に変更
+    change_column_null :trip_invitations, :sender_id, false
+  end
+
+  def down
+    remove_reference :trip_invitations, :sender
   end
 end
