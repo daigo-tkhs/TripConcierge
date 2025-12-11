7a5dd54bef30bdeba1fddca5f9df716e0bc08cb9
daigotechcareer-arch <daigo.tech.career@gmail.com>
2025-12-11 15:02:20 +0900

    Feature: 招待メール送信処理を完全に実装し、ビューファイルとメーラーを統合

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,28 +1,30 @@
+# app/mailers/user_mailer.rb (修正版全文)
 # frozen_string_literal: true
 
 class UserMailer < ApplicationMailer
+  # メーラー内でルーティングヘルパーを使うためにインクルード（環境依存のバグ対策）
+  include Rails.application.routes.url_helpers
+
   def welcome
     @greeting = 'Hi'
     # subject: '【TripConcierge】ようこそ！旅の準備を始めましょう' を置換
     mail to: params[:to], subject: t('messages.mail.welcome_subject')
   end
 
-  def invite_to_trip
-    @trip = params[:trip]
+  def invite_email
+    # with(...) で渡されたパラメータを取得
+    @invitation = params[:invitation]
     @inviter = params[:inviter]
-    @token = params[:invitation_token]
-    @invitation_url = url_for(
-      controller: 'invitations',
-      action: 'accept',
-      token: @token,
-      only_path: false
-    )
+    @trip = @invitation.trip
+    
+    @invite_url = invitation_url(@invitation.token)
 
-    # subject: "【TripConcierge】#{@inviter.nickname}さんから旅の招待状が届いています" を置換
+    # config/locales/ja.yml から件名を取得
+    subject = t('messages.mail.invite_subject', inviter_name: @inviter.nickname || @inviter.email)
+
     mail(
-      to: params[:to],
-      # i18nの subject に動的な値 (inviter_name) を渡します
-      subject: t('messages.mail.invite_subject', inviter_name: @inviter.nickname)
+      to: @invitation.email,
+      subject: subject
     )
   end
 end
