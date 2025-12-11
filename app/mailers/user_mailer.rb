aa11584f4de5dc5f1ab79eb6778114fecbabca7d
daigotechcareer-arch <daigo.tech.career@gmail.com>
2025-12-11 17:53:13 +0900

    Fix: ActionMailerのURL生成エラー解消のため、メーラー内でproduction設定を強制適用

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,25 +1,27 @@
-# app/mailers/user_mailer.rb
+# app/mailers/user_mailer.rb (最終確定版)
 # frozen_string_literal: true
 
 class UserMailer < ApplicationMailer
+  # ActionMailerのconfig/environments.rbの設定をジョブ実行環境に確実に適用させる
+  self.default_url_options = Rails.application.config.action_mailer.default_url_options
+  
   # メーラー内でルーティングヘルパーを使うためにインクルード（環境依存のバグ対策）
   include Rails.application.routes.url_helpers
 
   def welcome
     @greeting = 'Hi'
-    # subject: '【TripConcierge】ようこそ！旅の準備を始めましょう' を置換
     mail to: params[:to], subject: t('messages.mail.welcome_subject')
   end
 
   def invite_email
-    # with(...) で渡されたパラメータを取得
     @invitation = params[:invitation]
     @inviter = params[:inviter]
     @trip = @invitation.trip
     
+    # ここで self.default_url_options にホストとプロトコルが設定されているため、
+    # invitation_url は絶対URLを正しく生成できる
     @invite_url = invitation_url(@invitation.token)
 
-    # config/locales/ja.yml から件名を取得
     subject = t('messages.mail.invite_subject', inviter_name: @inviter.nickname || @inviter.email)
 
     mail(
