20cd895e4633cb6c4cdf2777de24e48a0183ad13
daigotechcareer-arch <daigo.tech.career@gmail.com>
2025-12-18 15:50:29 +0900

    Fix: Importmapのライブラリ実体をダウンロードし、manifestを確認

--- config/importmap.rb
+++ config/importmap.rb
@@ -8,7 +8,7 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 
 # その他のライブラリ
-pin "sortablejs" 
+pin "sortablejs" # @1.15.6
 pin "@rails/actioncable/src", to: "@rails--actioncable--src.js" # @8.1.100
-pin "debounce"
+pin "debounce" # @3.0.0
 pin "@hotwired/turbo", to: "@hotwired--turbo.js" # @8.0.20
